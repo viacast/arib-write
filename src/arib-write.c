37ec1a75e0eb2e389a1abb4a53f25fd67a3db88b
Lucas Clemente Vella <lvella@gmail.com>
2017-03-05 18:26:39 -0300

    Reading input groups in UTF-8 from stdin.

--- src/arib-write.c
+++ src/arib-write.c
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <assert.h>
 #include <pthread.h>
+#include <iconv.h>
 
 #include "timer.h"
 
@@ -53,7 +54,7 @@ static void *caption_writer_thread(void *par)
 
 static void subtitle_boilerplate(Buffer *data)
 {
-	uint8_t *buf = buffer_prepend(data, 53);
+	uint8_t *buf = buffer_prepend(data, 50);
 
 	size_t i = 0;
 
@@ -100,11 +101,11 @@ static void subtitle_boilerplate(Buffer *data)
 	buf[i++] = 0x50;
 
 	// APS (active position set), set to (13,0) (0x4d, 0x40)
-	buf[i++] = 0x1c;
+	/*buf[i++] = 0x1c;
 	buf[i++] = 0x4d;
-	buf[i++] = 0x40;
+	buf[i++] = 0x40;*/
 
-	assert(i == 53);
+	assert(i == 50);
 
 	data_unit(STATEMENT_1, STATEMENT_BODY, data);
 }
@@ -150,6 +151,24 @@ static void write_full_subtitle(FILE *out,
 	buffer_destroy(&data);
 }
 
+static size_t getline(char *buf, size_t size, FILE *f)
+{
+	size_t count = 0;
+	while(count < size) {
+		const int c = getc(f);
+		if(c == EOF) {
+			break;
+		}
+
+		buf[count++] = c;
+		if(c == '\n') {
+			break;
+		}
+	}
+	buf[count] = 0;
+	return count;
+}
+
 int main()
 {
 	pthread_t cwriter;
@@ -157,10 +176,39 @@ int main()
 	pthread_detach(cwriter);
 	sleep_for(0.5);
 
-	for(size_t i = 0;; ++i) {
+	iconv_t cd = iconv_open("l1", "utf8");
+
+	for(;;) {
 		char msg[4096];
-		snprintf(msg, 50, "\x1d\x21 pele de cobra %zd \x1d\x21", i);
-		write_full_subtitle(stdout, strlen(msg), msg);
-		sleep(10);
+		uint16_t count = 0;
+		uint8_t line_count = 0;
+		while(line_count < 2)
+		{
+			uint16_t ncount = count;
+			msg[ncount++] = 0x1c;
+			msg[ncount++] = 0x4d + line_count;
+			msg[ncount++] = 0x40;
+			size_t n;
+			{
+				size_t remsize = 4095 - ncount;
+				char buf[remsize];
+				size_t bn = getline(buf, remsize, stdin);
+
+				char *in = buf;
+				char *out = &msg[ncount];
+				iconv(cd, &in, &bn, &out, &remsize);
+				n = out - &msg[ncount];
+			}
+			if(msg[ncount] == '\n') {
+				if(line_count == 0) {
+					continue;
+				} else {
+					break;
+				}
+			}
+			count = ncount + n;
+			++line_count;
+		}
+		write_full_subtitle(stdout, count, msg);
 	}
 }
